ce9cd79d85179d8012ce87628c030ac6ec3463e3
abhishek.prasad <abhishek71994@gmail.com>
2025-02-05 18:15:03 +0800

    Added new shell info

--- shell.nix
+++ shell.nix
@@ -16,3 +16,26 @@ in
     echo "Hello World" > should_exist.txt
   '';
 }
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/06278c777
+b5d162e62df170fec307e83f1812d94b.tar.gz") {}
+}:
+
+-----
+
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    cowsay
+    lolcat
+    vim
+  ];
+  GREETING = "Hello there!";
+
+  shellHook = ''
+     echo $GREETING | cowsay | lolcat
+  '';
+}
